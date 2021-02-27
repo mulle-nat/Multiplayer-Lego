f804af8fdfff1a6f796b565b93871188d6655e3f
Nat! <nat@mulle-kybernetik.com>
2021-02-28 00:18:25 +0100

    Scenen rausgeworfen die wir nicht brauchen. Intro Menu mit Spieler auswahl. Aber Spieler ist noch in der Scene :)

---
+++ Assets/GameNetworkController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class GameNetworkController : MonoBehaviourPunCallbacks
+{
+    private void  Awake()
+    {
+       Vector3      position;
+       GameObject   spawn;
+       Quaternion   rotation;
+       
+       position = new Vector3( 241.92f, 1.0f, -42.6f); // default, aber falsch
+       rotation = Quaternion.identity; 
+       Debug.Log( "Nickname: \"" + PhotonNetwork.LocalPlayer.NickName + "\"");
+       spawn    = GameObject.Find( PhotonNetwork.LocalPlayer.NickName); 
+       if( spawn)
+       {
+          position = spawn.transform.position;
+          rotation = spawn.transform.rotation;
+       }
+       PhotonNetwork.Instantiate( "Spieler", position, rotation);
+    }
+}
+
