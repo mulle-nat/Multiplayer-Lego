dee6e028feebc7cf74b32adf4aae43b3484dd40f
Nat! <nat@mulle-kybernetik.com>
2021-02-28 01:57:58 +0100

    Logo figure spawnen je nach Auswahl auf drei verschiedenen Position in der Karte

--- Assets/GameNetworkController.cs
+++ Assets/GameNetworkController.cs
@@ -2,26 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Cinemachine;
 
 
 public class GameNetworkController : MonoBehaviourPunCallbacks
 {
+    public CinemachineFreeLook   freeLookCamera = null;
+
     private void  Awake()
     {
        Vector3      position;
        GameObject   spawn;
        Quaternion   rotation;
-       
+
        position = new Vector3( 241.92f, 1.0f, -42.6f); // default, aber falsch
-       rotation = Quaternion.identity; 
+       rotation = Quaternion.identity;
        Debug.Log( "Nickname: \"" + PhotonNetwork.LocalPlayer.NickName + "\"");
-       spawn    = GameObject.Find( PhotonNetwork.LocalPlayer.NickName); 
+       spawn    = GameObject.Find( PhotonNetwork.LocalPlayer.NickName);
        if( spawn)
        {
           position = spawn.transform.position;
           rotation = spawn.transform.rotation;
        }
-       PhotonNetwork.Instantiate( "Spieler", position, rotation);
+
+       GameObject player = PhotonNetwork.Instantiate( "Spieler", position, rotation);
+       if( ! player)
+       {
+          Debug.Log( "Did not get a player instantiated");
+          return;
+       }
+       if( ! freeLookCamera)
+       {
+          Debug.Log( "Did not get a camera hooked up in the editor");
+          return;
+       }
+       
+       freeLookCamera.Follow = player.transform;
+       freeLookCamera.LookAt = player.transform;
     }
 }
 
--- Assets/NetworkController.cs
+++ Assets/NetworkController.cs
@@ -11,57 +11,54 @@ public class NetworkController : MonoBehaviourPunCallbacks
     public Button        startButton = null;
     public TMP_Text      statusText = null;
     public TMP_Dropdown  spielerDropdown = null;
-    
+
     // Start is called before the first frame update
     void Start()
     {
        statusText.text = "Verbinde ...";
        PhotonNetwork.ConnectUsingSettings();
-       startButton.enabled = false;
+       startButton.gameObject.SetActive( false);
        Debug.Log( "Vrooom");
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log( "Ping");   
-    	base.OnConnectedToMaster();
-    	
-    	PhotonNetwork.AutomaticallySyncScene = true;
+        Debug.Log( "Ping");
+    	  base.OnConnectedToMaster();
+
+        PhotonNetwork.AutomaticallySyncScene = true;
         statusText.text = "Verbunden mit " +  PhotonNetwork.ServerAddress;
-	startButton.enabled = true;
+	     startButton.gameObject.SetActive( true);
     }
-    
+
     public override void OnJoinedRoom()
     {
-     	base.OnJoinedRoom();
-    	
-        Debug.Log( "Joined");  
-        SceneManager.LoadScene( "Lobby");
+     	  base.OnJoinedRoom();
+
+        Debug.Log( "Joined");
+        SceneManager.LoadScene( "Game");
         Debug.Log( "Clicky click");
      }
-    // Update is called once per frame
-    void Update()
-    {
-    }
-    
+
+
     public void startButton_Click()
     {
        string   name;
        int      index;
-       
+
        statusText.text = "Lade";
- 
+
        index = spielerDropdown.value;
        name  = spielerDropdown.options[ index].text;
-      
-       PhotonNetwork.LocalPlayer.NickName = name; 
-      
+
+       PhotonNetwork.LocalPlayer.NickName = name;
+
        Debug.Log( "Spieler #" + index + " : " + name);
        Photon.Realtime.RoomOptions   opts = new Photon.Realtime.RoomOptions();
        opts.IsOpen     = true;
        opts.IsVisible  = true;
        opts.MaxPlayers = 10;
-       
+
        PhotonNetwork.JoinOrCreateRoom( "Flobby", opts, Photon.Realtime.TypedLobby.Default);
     }
  }
